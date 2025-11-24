77837ac2e49d1bd512196af3615358ce8074616a
Devv1507 <davide.valencia.v@gmail.com>
2025-11-23 23:40:07 -0500

    fix: updated functions for api fetch with backend

--- src/shared/api/auth.js
+++ src/shared/api/auth.js
@@ -1,20 +1,41 @@
-// src/shared/api/auth.js
 import { apiFetch } from "./http";
 
+/**
+ * Función para iniciar sesión del usuario.
+ * @param {*} email - correo electrónico del usuario. E.g. 'pepito.perez@example.com'
+ * @param {*} password - contraseña del usuario. E.g. 'mi_contraseña_segura'
+ * @returns una promesa que resuelve los datos del usuario y token de autenticación.
+ */
 export function login(email, password) {
-  return apiFetch("/auth/login", {
+  return apiFetch("/users/login/", {
     method: "POST",
     body: JSON.stringify({ email, password }),
   });
 }
 
+/**
+ * Función para registrar un nuevo usuario.
+ * @param {*} userData - objeto con los datos del usuario. E.g. {
+ *  firs_name: 'Pepito',
+ *  last_name: 'Perez',
+ *  email: 'pepito.perez@example.com',
+ *  password: 'mi_contraseña_segura'
+ * }
+ * @returns una promesa que resuelve los datos del usuario registrado.
+ */
 export function register(userData) {
-  return apiFetch("/auth/register", {
+  return apiFetch("/users/register/", {
     method: "POST",
     body: JSON.stringify(userData),
   });
 }
 
+/**
+ * Función para cerrar la sesión del usuario.
+ * Se elimina el token de autenticación del almacenamiento local.
+ * @returns una promesa resuelta inmediatamente.
+ */
 export function logout() {
-  return apiFetch("/auth/logout", { method: "POST" });
+  localStorage.removeItem("/token/");
+  return Promise.resolve();
 }
