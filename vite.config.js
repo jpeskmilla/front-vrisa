69b2d52ca530a808a29c93906af833c4bbbc3290
Devv1507 <davide.valencia.v@gmail.com>
2025-11-23 23:39:25 -0500

    fix: updated vite settings to allow container hot-reload

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,21 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173, // Puerto interno de Vite
+    watch: {
+      usePolling: true,
+      interval: 100,
+      binaryInterval: 300,
+      ignored: ['**/node_modules/**', '**/.git/**'], 
+    },
+    hmr: {
+      host: 'localhost',
+      clientPort: 8088,  // Puerto externo mapeado
+      protocol: 'ws'
+    }
+  }
 });
+
